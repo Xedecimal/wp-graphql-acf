6d9c7cd1d7000a0215367a7fba483655e2c3af95
Nicholas Libby <Xedecimal@gmail.com>
2021-02-17 16:15:16 -0800

    Latest composer

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,6 +6,13 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'cli' => array($vendorDir . '/wp-cli/php-cli-tools/lib'),
+    'WP_CLI' => array($vendorDir . '/wp-cli/wp-cli/php'),
+    'Requests' => array($vendorDir . '/rmccue/requests/library'),
+    'Oxymel' => array($vendorDir . '/nb/oxymel'),
+    'Mustache' => array($vendorDir . '/mustache/mustache/src'),
     'LSS' => array($vendorDir . '/openlss/lib-array2xml'),
     'JsonMapper' => array($vendorDir . '/netresearch/jsonmapper/src'),
+    'Hautelook' => array($vendorDir . '/hautelook/phpass/src'),
+    'Behat\\Gherkin' => array($vendorDir . '/behat/gherkin/src'),
 );
